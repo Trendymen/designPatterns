debc8e7fee8cc0f05b9f1f6acb889f191fb72673
Trendymen <476212964@qq.com>
2021-05-25 00:23:29 +0800

    add java.util.Observable version.

--- src/headfirst/designPatterns/observer/weatherobservable/ForecastDisplay.java
+++ src/headfirst/designPatterns/observer/weatherobservable/ForecastDisplay.java
@@ -8,13 +8,26 @@ import java.util.Observable;
 
 
 public class ForecastDisplay implements Observer, DisplayElement {
+    private float lastPressure;
+    private float currentPressure = 29.2f;
+    private Observable observable;
+
+    public ForecastDisplay(Observable obs) {
+        this.observable = obs;
+        obs.addObserver(this);
+    }
+
     @Override
     public void update(Observable o, Object arg) {
-
+        if (o instanceof WeatherData weatherData) {
+            lastPressure = currentPressure;
+            currentPressure = weatherData.getPressure();
+            display();
+        }
     }
 
     @Override
     public void display() {
-
+        System.out.println(lastPressure + " " + currentPressure);
     }
 }
